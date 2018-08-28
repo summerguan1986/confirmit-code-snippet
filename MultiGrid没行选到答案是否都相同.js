14ba7df1b982217eb0821944c8299aca0798854d
Summer <34958150+summerguan1986@users.noreply.github.com>
2018-08-28 12:03:32 +0800

    Create MultiGrid没行选到答案是否都相同.js

---
+++ MultiGrid没行选到答案是否都相同.js
@@ -0,0 +1,19 @@
+((f('S12_1').get().toString()==f('S12_2').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_3').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_4').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_5').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_6').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_7').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_8').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_9').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_10').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_11').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_12').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_13').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_14').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_15').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_16').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_17').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_18').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_19').get().toString())&&
+ (f('S12_1').get().toString()==f('S12_20').get().toString()))
