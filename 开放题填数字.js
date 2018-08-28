35301b70a2603cb21fae4e05be5e88ef0c82e1f0
Summer <34958150+summerguan1986@users.noreply.github.com>
2018-08-28 10:48:39 +0800

    Update 开放题填数字.js

--- 开放题填数字.js
+++ 开放题填数字.js
@@ -8,3 +8,12 @@ if(f('Q2x13_3_other').toBoolean())
     SetErrorMessage(CurrentLang(),"请输入数字");
   }
 }
+
+
+//开放题只允许填数字，并且可保留一位小数
+var re = /^(\d+\.\d{1,1}|\d+)$/;
+if( !re.test(f('Q3x9_5_other').get()))
+{
+  RaiseError();
+  SetErrorMessage(CurrentLang(),"请输入数字，且最多保留一位小数");
+}
