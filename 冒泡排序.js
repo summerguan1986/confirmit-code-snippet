6d0c5278d61bc50da9343dbb9b9f4d43b2e8499a
Summer <34958150+summerguan1986@users.noreply.github.com>
2018-08-28 12:09:40 +0800

    Create 冒泡排序.js

---
+++ 冒泡排序.js
@@ -0,0 +1,13 @@
+//冒泡排序
+function sortArray(arr){
+  for(var i=0;i<arr.length-1;i++){
+    for(var j=0;j<arr.length-1-i;j++){
+      if(arr[j]>arr[j+1]){
+        var temp=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=temp;
+      }
+    }
+  }
+  return arr;
+}
