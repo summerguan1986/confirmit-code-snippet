ffdaa0fe2e9cbd79987ef95b9084c7b56a3da2f5
Summer <34958150+summerguan1986@users.noreply.github.com>
2018-08-28 10:28:05 +0800

    Update 开放题按顺序填写.js

--- 开放题按顺序填写.js
+++ 开放题按顺序填写.js
@@ -1,3 +1,4 @@
+//Method 1
 function SetItemFun() {
   var theobj;
   var queID;
@@ -16,3 +17,21 @@ function SetItemFun() {
 SetItemFun();
 var sQidStr = "A1B_";
 var row = 15;
+
+
+
+//Method 2,jQuery:
+//var formID = $("fieldset:eq(0)").attr("id").replace("fieldset_","");
+//:gt(index)  匹配所有大于给定索引值的元素
+var formID = $("fieldset:eq(0)").attr("id");
+var arr = $('#'+ formID +' input');
+arr.attr('disabled', true);
+arr.eq(0).attr('disabled', false);
+arr.keyup(function() {
+  var pos = arr.index($(this));
+  if ($(this).val()!="") {
+    arr.eq(pos+1).("disabled", false);
+  } else {
+    $('#' + formID +' input:gt('+pos+')').prop("disabled",true).val("");
+  }
+});
