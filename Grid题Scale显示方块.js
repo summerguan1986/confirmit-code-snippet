cff662b147ac9dcaaf387def887e9a27740e7d44
Summer <34958150+summerguan1986@users.noreply.github.com>
2018-08-28 11:47:46 +0800

    Create Grid题Scale显示方块.js

---
+++ Grid题Scale显示方块.js
@@ -0,0 +1,15 @@
+.page{color:#000;}
+.confirmit-gb-l{
+  text-align:center;
+}
+.confirmit-gb-star-container{
+  width:10%;
+}
+
+
+$(".ga").hide();
+
+
+$('.confirmit-gb-c').css("margin-left","0px");
+$('.confirmit-gb-c').css("width","100%");
+$('.confirmit-gb-c').find('table').css("width","100%");
