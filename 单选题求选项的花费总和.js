d76ed9c9e46099acc776611a43eca53505b76e78
Summer <34958150+summerguan1986@users.noreply.github.com>
2018-08-28 12:13:29 +0800

    Update 单选题求选项的花费总和.js

--- 单选题求选项的花费总和.js
+++ 单选题求选项的花费总和.js
@@ -1,2 +1,17 @@
 //如果单选题的Label是花费金额，并且需要求多个单选题选到的花费金额的总和，可用parseInt()函数
 f('sumCOST').set(parseInt(f('Q1').valueLabel()) + parseInt(f('Q2').valueLabel()));
+
+
+
+//计算消费总额，其中消费选项为消费段，并且包含其他字符
+//parseInt返回字符串中的第一个数字，直至非数字字符为止
+if(f('Q1') != '99') {
+  if(f('Q1').any('1','7','8','14','15','21','22','28','29','35','36',
+   '42','43','49','50','56','57','63','64','70')) {
+    sum1 = sum1 + (parseInt(f('Q1').valueLabel()) *12)
+} else {
+  var code1 = f('Q1').valueLabel();
+  var array1 = code1.split("-");
+  sum1 = sum1 + ( ( (parseInt(array1[0]) + parseInt(array1[1]) ) / 2 ) * 12 )
+}
+}
