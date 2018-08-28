d3bc37335989d7f106214287ac329d523b59af41
Summer <34958150+summerguan1986@users.noreply.github.com>
2018-08-28 11:54:56 +0800

    Create 禁用鼠标右键.js

---
+++ 禁用鼠标右键.js
@@ -0,0 +1,9 @@
+//禁用鼠标右键
+//d2n88fe5uqdqty.cloudfront.net/rnd/plugins/imageprotection/imageProtection.min.js
+
+//禁用鼠标右键
+$(document).ready(function(){  
+  $(document).bind("contextmenu",function(e){   
+    return false;   
+  });
+});
