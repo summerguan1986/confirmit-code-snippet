7f4eb18ea763beb75dd94bc5ed883852734312b9
summerguan1986 <254935649@163.com>
2019-07-06 17:36:32 +0800

    add code

--- 3DGrid-按照组头出示表头.js
+++ 3DGrid-按照组头出示表头.js
@@ -3,5 +3,8 @@ for(var i = 1; i <= $('th.scale').length; i++) {
     for(j = 1; j <= 5; j++) {
         $("th.grid-group-header-label").eq(j).parent().append('<td>' + $("#Q2A1_header" + i).text() + '</td>');            
     }
+}
 
+function giveitup() {
+    
 }
